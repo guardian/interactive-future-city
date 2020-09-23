e8c9f6c82f16faf5aea85692a050fb0115c4aa30
superfrank <frank.hulley-jones@guardian.co.uk>
2020-09-23 09:33:03 +0100

    all firms in

---
+++ atoms/body-drmm/server/render.js
@@ -0,0 +1,5 @@
+import mainHTML from "./atoms/body-drmm/server/templates/main.html!text"
+
+export async function render() {
+    return mainHTML;
+} 
---
+++ atoms/header-drmm/server/render.js
@@ -0,0 +1,5 @@
+import mainHTML from "./atoms/header-drmm/server/templates/main.html!text"
+
+export async function render() {
+    return mainHTML;
+} 
